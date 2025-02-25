0c47f88c0cc5934c1cf6754b64ddfb8cc08de809
Paul356 <panhao356@gmail.com>
2025-02-25 15:26:18 +0800

    Add config page, save current page in sessionStorage

--- web/src/views/Config.vue
+++ web/src/views/Config.vue
@@ -1,18 +1,114 @@
 <template>
-  <div class="config">
-    <h1>Config</h1>
-    <p>This is an example of a Single Page Application using Vue.js.</p>
+<div class="config">
+  <h1>WiFi设置</h1>
+  <div class="wifi_container">
+    <label class="config_title">工作模式</label>
+    <select id="mode_select">
+      <option value="UNKNOWN" v-bind:selected="wifi_config['mode'] == 'UNKNOWN'">--请选择--</option>
+      <option value="AP" v-bind:selected="wifi_config['mode'] == 'AP'">WiFi热点</option>
+      <option value="STA" v-bind:selected="wifi_config['mode'] == 'STA'">客户端</option>
+      <option value="NULL" v-bind:selected="wifi_config['mode'] == 'NULL'">关闭WiFi</option>
+    </select>
+    <label class="config_title">SSID</label><input type="text" id="ssid_input" v-bind:value="wifi_config['ssid']" />
+    <label class="config_title">密码</label><input type="password" id="passwd_input" v-bind:value="wifi_config['passwd']"/>
   </div>
+  <br/>
+  <button v-on:click="submit_wifi_config()">提交修改</button>
+</div>
 </template>
 
 <script>
+import { ref } from 'vue';
+import axios from 'axios';
+
 export default {
-  name: 'Config'
+    setup() {
+        const wifi_config = ref({"mode": "UNKOWN", "ssid": "", "passwd": ""});
+
+        function update_wifi_config() {
+            const wifi_uri = "/device/wifi";
+            axios.get(wifi_uri)
+                .then(function (response) {
+                    console.log(`get response ${response.data}`);
+                    var config = Object.assign({}, response.data);
+                    config['passwd'] = "********";
+                    wifi_config.value = config;
+                })
+                .catch(function (error) {
+                    window.alert(`fail to get wifi config, error=${error}`);
+                    wifi_config.value = {"mode": "UNKOWN", "ssid": "", "passwd": "********"};
+                });
+        }
+
+        function submit_wifi_config() {
+            const mode_select = document.getElementById("mode_select");
+            const ssid_input = document.getElementById("ssid_input");
+            const passwd_input = document.getElementById("passwd_input");
+
+            const selected_mode = mode_select.options[mode_select.selectedIndex].value;
+            if (selected_mode == "UNKNOWN") {
+                window.alert("Please select a mode.");
+                return;
+            }
+
+            const ssid = ssid_input.value.trim();
+            const passwd = passwd_input.value.trim();
+            if (selected_mode != "NULL" && ssid == "") {
+                window.alert("SSID should not be empty");
+                return;
+            }
+
+            const wifi_config_json = {
+                "mode" : selected_mode,
+                "ssid" : ssid,
+                "passwd" : passwd
+            };
+
+            mode_select.disabled = true;
+            ssid_input.disabled = true;
+            passwd_input.disabled = true;
+            const config_wifi_uri = "/device/wifi";
+            axios.put(config_wifi_uri, wifi_config_json)
+                .then((response) => {
+                    console.log("wifi config is updated");
+                    wifi_config.value = {
+                        "mode" : selected_mode,
+                        "ssid" : ssid,
+                        "passwd" : "********"
+                    };
+                })
+                .catch((error) => {
+                    window.alert(`can't change wifi config, error=${error}`);
+                })
+                .finally(() => {
+                    mode_select.disabled = false;
+                    ssid_input.disabled = false;
+                    passwd_input.disabled = false;
+                });
+        }
+
+        update_wifi_config();
+        return {
+            wifi_config,
+            submit_wifi_config,
+        };
+    }
 };
 </script>
 
 <style scoped>
 h1 {
   color: #42b983;
+}
+.wifi_container {
+    display: grid;
+    grid-template-columns: 70px 120px;
+    grid-template-rows: auto auto auto;
+    justify-content: center;
+    grid-gap: 1px;
+}
+.config_title {
+    background-color: #42b983;
+    color: white;
 }
 </style>
--- web/src/views/FileExplorer.vue
+++ web/src/views/FileExplorer.vue
@@ -1,37 +1,71 @@
 <template>
 <div class="file_explorer">
-  <h1>文件浏览器</h1>
+  <br/>
+  <div class="path_container">
+    <button v-for="path in path_list" v-on:click="goto_directory(path)" class="path_btn borderless">{{ path.length > 1 ? path.substr(path.lastIndexOf("/", path.length - 2) + 1) : path }}</button>
+  </div>
+  <br/>
   <table>
     <tr>
       <th class="name_col">文件名</th>
-      <th class="size_col">文件大小</th>
+      <th class="size_col">大小</th>
       <th class="time_col">修改时间</th>
       <th class="action_col">操作</th>
     </tr>
-    <tr v-for="item in this.file_list">
+    <tr v-for="item in file_list">
       <td class="name_col">{{ item.name }}</td>
       <td class="size_col">{{ item.size }}</td>
       <td class="time_col">{{ item.mtime }}</td>
-      <td class="action_col"><button v-on:click="click_download(item.name)" class="boardless">{{ item.name.endsWith("/") ? "进入" : "下载" }}</button> <button v-on:click="click_delete(item.name)" class="boardless">删除</button></td>
+      <td class="action_col">
+        <button v-on:click="click_download(item.name, $event)" class="borderless download_btn">{{ item.name.endsWith("/") ? "进入" : "下载" }}</button> <button v-on:click="click_delete(item.name, $event)" class="borderless delete_btn">删除</button>
+      </td>
     </tr>
   </table>
-  <button v-on:click="create_subdir()" class="boardless">新建目录</button><label>{{ curr_path }}</label><input type="text" id="dir_input" v-on:keyup.enter="create_subdir()" />
+  <br/>
+  <div class="upload_container">
+    <button v-on:click="create_subdir()" class="borderless" id="dir_button">新建目录</button>
+    <label>{{ curr_dir }}</label>
+    <input type="text" id="dir_input" v-on:keyup.enter="create_subdir()" />
+    <button v-on:click="upload_file()" class="borderless" id="file_btn" >上传文件</button>
+    <label>{{ curr_dir }}</label>
+    <input type="file" id="file_input" />
+  </div>
+  <a style="display: none;" href="#" download="" id="download_anchor"></a>
 </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import axios from 'axios';
 
 export default {
     setup() {
-        const curr_path = ref("/");
+        const curr_dir_key = "curr_directory";
+        const curr_dir = ref(sessionStorage.getItem("curr_directory") || "/");
+        function update_curr_dir(new_dir) {
+            curr_dir.value = new_dir;
+            sessionStorage.setItem(curr_dir_key, new_dir);
+        }
+
+        const path_list = computed(() => {
+            const paths = curr_dir.value.split("/").filter((tok) => tok != "");
+            if (paths.length == 0) {
+                return ["/"];
+            } else {
+                return paths.reduce((accumu, curr) => {
+                    accumu.push(accumu.at(-1) + curr + "/");
+                    return accumu;
+                }, ["/"]);
+            }
+        });
+
         const file_list = ref([]);
 
-        function get_file_list(file_path) {
-            axios.get(`/get${file_path}`)
+        function update_file_list(file_path) {
+            const get_uri = encodeURI(`/get${file_path}`);
+            axios.get(get_uri)
                 .then(function (response) {
-                    console.log(`get response.data ${response.data}`);
+                    console.log(`get response ${response.data}`);
                     file_list.value = response.data;
                 })
                 .catch(function (error) {
@@ -40,64 +74,125 @@ export default {
                 });
         }
 
-        function click_download(file_name) {
+        function goto_directory(directory_path) {
+            update_curr_dir(directory_path);
+            update_file_list(directory_path);
+        }
+
+        function click_download(file_name, event) {
             if (file_name.endsWith("/")) {
-                curr_path.value = curr_path.value + file_name;
-                get_file_list(curr_path.value)
+                const new_dir = curr_dir.value + file_name;
+                update_curr_dir(new_dir);
+                update_file_list(new_dir);
             } else {
-                const download_uri = encodeURI('/get' + curr_path.value + file_name);
-                axios.get(download_uri)
-                    .then(function (response) {
-                        console.log(`File ${download_uri} is downloaded`);
-                    })
-                    .catch(function (error) {
-                        window.alert(`can't download ${file_name}, error=${error}`)
-                    });
+                event.target.disabled = true;
+                const download_uri = encodeURI('/get' + curr_dir.value + file_name);
+                const a = document.getElementById('download_anchor');
+                a.href = download_uri;
+                a.click();
+                event.target.disabled = false;
             }
         }
 
-        function click_delete(file_name) {
-            const delete_uri = encodeURI('/delete' + curr_path.value + file_name);
+        function click_delete(file_name, event) {
+            const delete_uri = encodeURI('/delete' + curr_dir.value + file_name);
+            event.target.disabled = true;
             axios.delete(delete_uri)
                 .then(function (response) {
                     console.log(`File ${delete_uri} is deleted`);
-                    get_file_list(curr_path.value); // optimize by remove entry directly
+                    update_file_list(curr_dir.value); // optimize by remove entry directly
                 })
                 .catch(function (error) {
                     window.alert(`can't delete ${file_name}, error=${error}`)
+                })
+                .finally(function () {
+                    event.target.disabled = false;
                 });
         }
 
         function create_subdir() {
             const dir_input = document.getElementById("dir_input");
+            const dir_button = document.getElementById("dir_button");
             const dir_name = dir_input.value.trim();
             const toks = dir_name.split("/");
             const filtered = toks.filter((tok) => tok != "")
-            const create_uri = encodeURI('/upload' + curr_path.value + filtered.join("/") + "/");
+
+            const filtered_name = filtered.join("/");
+            if (filtered_name.length == 0) {
+                window.alert(`dir "${dir_name}" is not valid.`);
+                return;
+            }
+
+            const create_uri = encodeURI('/upload' + curr_dir.value + filtered_name + "/");
+            dir_input.disabled = true;
+            dir_input.disabled = true;
             axios.post(create_uri)
                 .then(function (response) {
                     console.log(`Directory ${dir_name} is created`);
-                    get_file_list(curr_path.value);
+                    update_file_list(curr_dir.value);
                 })
                 .catch(function (error) {
                     window.alert(`can't create dir ${dir_name}, error=${error}`)
+                })
+                .finally(function () {
+                    dir_button.disabled = false;
+                    dir_input.value = "";
+                    dir_input.disabled = false;
                 });
         }
 
-        get_file_list(curr_path.value);
+        function upload_file() {
+            const file_input = document.getElementById("file_input");
+            const file_btn = document.getElementById("file_btn");
+
+            const sel_files = file_input.files;
+            if (sel_files.length == 0) {
+                window.alert("未选择文件");
+                return;
+            }
+
+            const file_name = sel_files[0].name;
+            if (file_name[file_name.length - 1] == '/') {
+                window.alert("不支持上传整个目录");
+                return;
+            }
+
+            file_input.disabled = true;
+            file_btn.disabled = true;
+
+            const upload_url = encodeURI("/upload" + curr_dir.value + file_name);
+            axios.post(upload_url, sel_files[0])
+                .then((response) => {
+                    console.log(`file ${file_name} is uploaded`);
+                    update_file_list(curr_dir.value);
+                })
+                .catch((error) => {
+                    window.alert(`can't upload file ${file_name}, error=${error}`);
+                })
+                .finally(() => {
+                    file_input.disabled = false;
+                    file_btn.disabled = false;
+                    file_input.value = "";
+                });
+        }
+
+        update_file_list(curr_dir.value);
         return {
-            curr_path,
+            curr_dir,
+            path_list,
             file_list,
+            goto_directory,
             click_download,
             click_delete,
             create_subdir,
+            upload_file,
         };
     },
 };
 </script>
 
 <style scoped>
-h1 {
+label {
   color: #42b983;
 }
 table {
@@ -107,8 +202,37 @@ table {
 }
 th, tr, td {
     border: 1px solid;
+    border-color: #dddddd;
 }
 td {
     text-align: left;
+}
+.path_container {
+    flex-grow: 0;
+    flex-direction: row;
+    gap: 1px;
+}
+.upload_container {
+    display: grid;
+    grid-template-columns: 70px auto 200px;
+    grid-template-rows: auto auto;
+    justify-content: center;
+    grid-gap: 2px;
+}
+.path_btn {
+    font-size: 24px;
+    background-color: #42b983;
+    color: white;
+}
+.borderless {
+    border: 0px;
+    background-color: #42b983;
+    color: white;
+}
+.borderless:hover {
+    background-color: #215cc1;
+}
+.delete_btn:hover {
+    background-color: #ff0000;
 }
 </style>
